0a120db2097186ad6c6724891bed9eb70a98b87c
gcarrghub <59971997+gcarrghub@users.noreply.github.com>
2020-01-16 19:20:19 -0500

    Add files via upload

--- shinyFromGIT.R
+++ shinyFromGIT.R
@@ -1,17 +1,23 @@
-# Check for required packages and install if necessary
+####
+#### https://github.com/gcarrghub/BV-Shiny
+####
+# Run these lines to check for required packages and install if necessary
+# Typically should only need to do this once
 packages = c("shiny", "dplyr", "gridExtra", "openxlsx", "optimx", "plotrix")
 packageTests <- sapply(packages,FUN = require,character.only=TRUE)
 install.packages(packages[!packageTests], repos = "https://cran.rstudio.com/")
 
-#with the right packages installed, the tool will run with only these two lines
+### once the packages above are installed
+### the tool will run with only these two lines
+# If chrome or firefox are your default browsers,
+# this should work fine, even in a terminal or Rgui
 library(shiny)
-# If chrome or firefox are your default browsers, this should work fine, even in
-# a terminal or Rgui
 runGitHub("BV-Shiny", "gcarrghub",launch.browser=TRUE)
 
 # If Firefox or Chrome are not default browser, open tool with
-# the line inside this if(FALSE){...}, then copy the http web address
+# the two lines inside this if(FALSE){...}, then copy the http web address
 # from the basic browser window into chrome or firefox address bar
 if(FALSE){
+  library(shiny)
   runGitHub("BV-Shiny", "gcarrghub",launch.browser = .rs.invokeShinyWindowViewer)
 }
