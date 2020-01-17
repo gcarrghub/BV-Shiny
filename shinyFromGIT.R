01f56ab6cd8b4855b2677bfe9a73aa9afbbeb9a0
gcarrghub <gregory.j.carr@gmail.com>
2020-01-17 09:27:22 -0500

    Added windows browser options

--- shinyFromGIT.R
+++ shinyFromGIT.R
@@ -24,11 +24,22 @@ runGitHub("BV-Shiny", "gcarrghub",launch.browser=TRUE)
 
 if(FALSE){
   ### If Firefox or Chrome are not default browser, open tool with
-  ### the two lines inside this if(FALSE){...}, then copy the http web address
-  ### from the basic browser window into chrome or firefox address bar
+  ### the following two lines, then copy the http web address
+  ### from the resulting basic browser window into chrome or firefox address bar
   ###
   ### At least on mac, instead of copy-paste you could alternatively highlight 
   ### the whole address and drag it into an open browser window too
   library(shiny)
+  ### NOTE:  This will ONLY work in Rstudio
   runGitHub("BV-Shiny", "gcarrghub",launch.browser = .rs.invokeShinyWindowViewer)
+  
+  ### If something other than Chrome or Firefox is your default browser and you don't want to change it,
+  ### another option on windows, not mac, may be to set a browser option first as follows
+  ### The location in the following command may need to be modified to work on your system
+  ### It is up to you to find where chrome.exe or firefox.exe is located if these don't work
+  library(shiny)
+  ### only use one of the following two depending on your preference.
+  options(browser = "C:/Program Files/Google/Chrome/Application/chrome.exe")
+  options(browser = "C:/Program Files/Mozilla Firefox/firefox.exe")
+  runGitHub("BV-Shiny", "gcarrghub",launch.browser=TRUE)
 }
