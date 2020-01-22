08c8dfcc23b40111d22bc3a202179d445aa70a6f
gcarrghub <gregory.j.carr@gmail.com>
2020-01-22 07:44:55 -0500

    Move logo files up

--- ui.R
+++ ui.R
@@ -93,7 +93,7 @@ shinyUI(
                                                    tags$li("Primary: Gregory Carr (carr.gj@pg.com)"),
                                                    tags$li("Secondary: Joel Chaney (chaney.jg@pg.com)")
                                               ),
-                                              img(src="PG-DMS.png", 
+                                              img(src="../PG-DMS.png", 
                                                   #this file should be in the www folder, not top level
                                                   #or maybe ../D&MS_Logo_RGB-Color.png would work?
                                                   height=100)
