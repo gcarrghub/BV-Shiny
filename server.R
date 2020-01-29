848c91771b7fdcd7d856b94fb2fc95caece10c6b
gcarrghub <gregory.j.carr@gmail.com>
2020-01-29 14:19:06 -0500

    minor

--- server.R
+++ server.R
@@ -511,7 +511,7 @@ shinyServer(function(input, output, session, clientData) {
      ### (I think) whenever the data change, update the data tab
      inputChanges <- reactive({list(dataOrgZeroFixed(),dataOrg(),input$ECXvalue,input$varFixed)})
      observeEvent(inputChanges(), {
-             print(rv$amsg)
+             #print(rv$amsg)
              rv$amsg <- ""
              output$plot <- NULL
              output$resultsTable <- NULL
